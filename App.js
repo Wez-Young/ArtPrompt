bcdd9ce96faa053f97ac9380f09b80c81f3dcdcf
Wez-Young <oldwezyoung@gmail.com>
2022-08-12 11:24:05 +0100

    Begun styling the UI

--- App.js
+++ App.js
@@ -1,31 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ColourPaletteScreen from './screens/ColourPaletteScreen';
-import TriadicColourPaletteScreen from './screens/TradicColourPaletteScreen';
-import HomeScreen from './screens/HomeScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import ColourTheoryPaletteScreen from "./screens/ColourTheoryPaletteScreen";
 
 export default function App() {
   const Stack = createStackNavigator();
 
   return (
-    <NavigationContainer >
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name='ColourPalette' component={ColourPaletteScreen}/>
-        <Stack.Screen name='TriadicPalette' component={TriadicColourPaletteScreen}/>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#161616",
+            shadowColor: "transparent",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerLeftLabelVisible:false,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Prompt: Art",
+          }}
+        />
+        <Stack.Screen
+          name="ColourTheoryPalette"
+          component={ColourTheoryPaletteScreen}
+          options={{
+            headerTitle: "Colour Palettes",
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
