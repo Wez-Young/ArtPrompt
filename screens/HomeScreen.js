439b92f3c6d678867aca0a0ecc60f5fffda73077
Wez-Young <oldwezyoung@gmail.com>
2022-08-12 11:24:27 +0100

    Added 'buttons to current and future screens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,50 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, SafeAreaView, TouchableOpacity, StyleSheet } from "react-native";
+
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Button
-        title="Colour Palette"
+    <View style={styles.mainContainer}>
+      <TouchableOpacity
+        style={styles.buttonStyle}
         onPress={() => {
-          navigation.navigate("ColourPalette");
+          navigation.navigate("ColourTheoryPalette");
         }}
-      />
-      <Button
-        title="Triadic Palette"
+      >
+        <Text style={styles.buttonText}>Colour Palettes</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonStyle}
         onPress={() => {
-          navigation.navigate("TriadicPalette");
+          navigation.navigate("ColourTheoryPalette");
         }}
-      />
+      >
+        <Text style={styles.buttonText}>Subject Prompts</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={() => {
+          navigation.navigate("ColourTheoryPalette");
+        }}
+      >
+        <Text style={styles.buttonText}>Saved Prompts</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: { flex: 1, backgroundColor: "#161616" },
+  buttonStyle: {
+    padding: 12,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    backgroundColor: "#EFF0F8",
+    height: "20%",
+
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
